de545faa0e6ecfbf0a34ea1abeba7c7a703f4502
quentin saval <quentin.saval@hotmail.fr>
2024-02-05 10:39:18 +0100

    5/02/2024,10:39

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,8 +15,30 @@ class MainController extends AbstractController
     #[Route('/', name: 'main')]
     public function index(CategoryRepository $categoryRepository): Response
     {
+
+        $categories = $categoryRepository->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'product')]
+    public function product(ProductRepository $productRepository, Category $category): Response
+    {
+
+        $products = $productRepository->findBy(['id_category' => $category->getId()]);
+
+        return $this->render('main/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/detail/{id}', name: 'detail')]
+    public function detail(Product $product): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'product' => $product,
         ]);
     }
 }

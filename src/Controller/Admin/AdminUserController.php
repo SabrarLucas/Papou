5e08d73445e6b2c8f10bbafa0f3654d9a0ce9b3c
SabrarLucas <pinchonlucas03@gmail.com>
2024-02-27 20:33:45 +0100

    generation du qr-code pour les partenaires

--- src/Controller/Admin/AdminUserController.php
+++ src/Controller/Admin/AdminUserController.php
@@ -3,14 +3,19 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-use App\Entity\Product;
-use App\Entity\Supplier;
 use App\Form\UserType;
+use App\Entity\Supplier;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Color\Color;
 use App\Repository\UserRepository;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Encoding\Encoding;
 use Doctrine\ORM\EntityManagerInterface;
+use Endroid\QrCode\ErrorCorrectionLevel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/user', name: 'admin_user_')]
@@ -54,5 +59,36 @@ class AdminUserController extends AbstractController
         $em->flush();
         
         return $this->render('admin/index.html.twig');
+    }
+
+    #[Route('/qrcode/{id}', name: 'qr_code')]
+    public function qrCode(User $user): Response
+    {
+        $id = $user->getSuppliers()[0]->getId();
+
+        // Vérification si l'ID du fournisseur est valide
+        if (!$id) {
+        // Gérer le cas où l'ID du fournisseur est vide
+            throw new \InvalidArgumentException("ID of supplier is empty.");
+        }
+
+        $url = $this->generateUrl('partner_index', ['id' => $id], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $writer = new PngWriter();
+        $qrCode = QrCode::create($url)
+            ->setEncoding(new Encoding('UTF-8'))
+            ->setErrorCorrectionLevel(ErrorCorrectionLevel::Low)
+            ->setSize(400)
+            ->setMargin(0)
+            ->setForegroundColor(new Color(0, 0, 0))
+            ->setBackgroundColor(new Color(255, 255, 255));
+
+        $qrCodes = [];
+        $qrCodes['simple'] = $writer->write(
+                                $qrCode,
+                                null,
+                            )->getDataUri();
+
+        return $this->render('admin/user/qr_code.html.twig', $qrCodes);
     }
 }

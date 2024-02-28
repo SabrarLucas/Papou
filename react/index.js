84896d1c13a6fb3ba97361c992a59427df5f004f
quentin saval <quentin.saval@hotmail.fr>
2024-02-28 08:36:20 +0100

    ajout d'api platform et de react et debut de la page commande de espace admin

---
+++ react/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Order from './compotent/Order'
+
+ReactDOM.createRoot(document.getElementById('react-app')).render(
+    <React.StrictMode>
+        <Order />
+    </React.StrictMode>,
+)

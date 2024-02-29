2fef44af9a3ef389a1084727d6d6d62c05f98ca3
quentin saval <quentin.saval@hotmail.fr>
2024-02-29 09:58:59 +0100

    correction des erreur

--- config/bundles.php
+++ config/bundles.php
@@ -16,10 +16,7 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-<<<<<<< HEAD
-    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
-=======
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
->>>>>>> quentin
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];

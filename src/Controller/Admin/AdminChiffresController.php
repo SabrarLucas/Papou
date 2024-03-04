f65f39e245e4a98eb712bce22cacd39af067fa1f
SabrarLucas <pinchonlucas03@gmail.com>
2024-03-04 17:43:02 +0100

    + de chiffres

--- src/Controller/Admin/AdminChiffresController.php
+++ src/Controller/Admin/AdminChiffresController.php
@@ -2,24 +2,76 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Product;
+use App\Entity\Order;
+use App\Repository\UserRepository;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 #[Route('/admin/chiffres', name: 'admin_chiffres_')]
 class AdminChiffresController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(
+        ProductRepository $productRepository,
+        UserRepository $userRepository,
+        EntityManagerInterface $em,
+        SessionInterface $session
+    ): Response
     {
-        $products = $productRepository->findAll();
+        //------Le nombre de produits------
+        $nbrProduct = count($productRepository->findAll());
 
-        $nbrProduct = count($products);
+        $repository = $em->getRepository(Order::class); //Le repository de la classe des commandes
+        
+        $debutDeLaJournee = new \DateTime('today'); //La date du jour
+
+        //------Nombres de commandes à partir de 00h------
+        $nbrCommandes = $repository->createQueryBuilder('c')
+            ->select('COUNT(c.id)')
+            ->where('c.created_at >= :debutDeLaJournee')
+            ->setParameter('debutDeLaJournee', $debutDeLaJournee)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        //------Le chiffre d'affaire à partir de 00h------
+        $CA = $repository->createQueryBuilder('c')
+            ->select('SUM(c.total)')
+            ->where('c.created_at >= :debutDeLaJournee')
+            ->setParameter('debutDeLaJournee', $debutDeLaJournee)
+            ->getQuery()
+            ->getSingleScalarResult();
+        
+        //------Le nombres d'utilisateurs inscrits------
+        $nbrUser = count($userRepository->findAll());
+
+        //------Le nombre de personnes connectées------
+        $sessionData = $session->all();
+        // Comptez le nombre de sessions avec la clé "is_logged_in" définie à true
+        $nbrUserLog = array_count_values(array_column($sessionData, 'is_logged_in'))[true] ?? 0;
+
+        //------Le nombre d'abonnées Instagram------
+        // $url = "https://www.instagram.com/therock";
+
+        // $response = file_get_contents($url . '/?__a=1');
+        // $data = json_decode($response, true);
+
+        // $nbrInsta = 0;
+
+        // if (isset($data['user']['followed_by']['count'])) {
+        //     $nbrInsta = $data['user']['followed_by']['count'];
+        // }
 
         return $this->render('admin/chiffres/index.html.twig', [
-            'nbrProduct' => $nbrProduct
+            'nbrProduct' => $nbrProduct,
+            'nbrUser' => $nbrUser,
+            'nbrUserLog' => $nbrUserLog,
+            // 'nbrInsta' => $nbrInsta,
+            'nbrCommandes' => $nbrCommandes,
+            'CA' => $CA
         ]);
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,16 +5,18 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     #[Route(path: '/new-partner', name: 'app_login_first_connection')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, SessionInterface $sessionInterface): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('main');
+            $sessionInterface->set('is_logged_in', true);
         }
 
         if ($_SERVER['REQUEST_URI'] == '/new-partner') {

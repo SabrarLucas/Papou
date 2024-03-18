24830ac91d0578c1e9a29487082268c7605eac37
quentin saval <quentin.saval@hotmail.fr>
2024-03-18 15:55:44 +0100

    ajout d'une page de modification de profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,23 +15,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class UserController extends AbstractController
 {
     #[Route('/{id}', name: 'profil')]
-    public function profil(User $user, Request $request, EntityManagerInterface $manager): Response
+    public function profil(User $user): Response
     {
         if($user == $this->getUser()){
 
-            $form = $this->createForm(UserType::class, $user); // creation du formulaire
-
-            $form->handleRequest($request);
-
-            if($form->isSubmitted() && $form->isValid()){
-                $user = $form->getData(); // ajout des donnée du formulaire dans l'objet user
-
-                $manager->persist($user); 
-                $manager->flush(); // envoie de l'objet dans la bbd 
-            }
-            return $this->render('user/profil.html.twig', [
-                'form' => $form, // envoie du formulaire a la vue
-            ]);
+            return $this->render('user/profil.html.twig');
         }
         return $this->redirectToRoute('main'); // retour a l'acceuil du site
     }
@@ -60,6 +48,31 @@ class UserController extends AbstractController
             $this->container->get('security.token_storage')->setToken(null); // on retire le token
 
             return $this->redirectToRoute('main'); // retour a l'acceuil du site
+        }
+        return $this->redirectToRoute('main'); // retour a l'acceuil du site
+    }
+
+    #[Route('/{id}/edit', name: 'edit')]
+    public function edit (User $user, EntityManagerInterface $manager, Request $request): Response
+    {
+        if($user == $this->getUser()){
+
+            
+            $form = $this->createForm(UserType::class, $user); // creation du formulaire
+
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $user = $form->getData(); // ajout des donnée du formulaire dans l'objet user
+
+                $manager->persist($user); 
+                $manager->flush(); // envoie de l'objet dans la bbd
+                return $this->redirectToRoute('user_profil'); // retour a l'acceuil du site 
+            }
+
+            return $this->render('user/profilEdit.html.twig', [
+                'form' => $form, // envoie du formulaire a la vue
+            ]);
         }
         return $this->redirectToRoute('main'); // retour a l'acceuil du site
     }
